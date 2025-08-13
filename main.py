940f5f6ec26cf953c515e880a997163e0d4044dd
Dory <pemegomachado@gmail.com>
2025-08-13 16:22:50 +0200

    Added euler's number

--- main.py
+++ main.py
@@ -31,6 +31,10 @@ def pi(iterations: int = 100) -> Decimal:
         sum += Decimal(1 / (i * i))
     return square_root(6 * sum, 0, int(iterations / 10))
 
+def euler(iterations: int = 100) -> Decimal:
+    sum = 1
+    return power(1 + Decimal(1 / iterations), iterations)
+
 if __name__ == "__main__":
     print("Welcome to the Irrational Number Calculator!")
     print("This calculator will calculate irrational numbers like pi and sqrt(2), with 100 decimal digits!")
@@ -39,6 +43,7 @@ if __name__ == "__main__":
     print("")
     print("1) Square Root of 2")
     print("2) Pi")
+    print("3) Euler's Number")
 
     response = input("")
     match response:
@@ -55,4 +60,10 @@ if __name__ == "__main__":
             print("We're gonna calculate pi using the sum of the reciprocals of the squares of the first n natural numbers")
             while True:
                 print(f"{i} iterations: {pi(i)}")
+                i *= 10
+        case "3":
+            i = 10
+            print("We're gonna calculate euler's number using the sum of the reciprocals of the first n natural numbers")
+            while True:
+                print(f"{i} iterations: {euler(i)}")
                 i *= 10
