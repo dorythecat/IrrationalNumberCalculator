bbd534c3162278441894c828464b830dab27cc17
Dory <pemegomachado@gmail.com>
2025-08-13 14:50:22 +0200

    Added pi algorithm

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ def square_root(x: float, guess: float = 0, iterations: int = 10):
 def factorial(x: float):
     if x == 1 or x == 2:
         return x
-    return (x * x - x) * factorial(x - 2)
+    return x * factorial(x - 1)
 
 def power(x: float, y: int):
     if y == 0:
         return 1
     return x * power(x, y - 1)
+
+def pi(iterations: int = 10):
+    sum = 0
+    for i in range(1, iterations):
+        sum += 1 / (i * i)
+    return square_root(6 * sum)
