6fe30dbf33c5631ddbfbc140f5e95eebd3f90259
Dory <pemegomachado@gmail.com>
2025-08-13 14:05:22 +0200

    Added factorial

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def square_root(x: float, guess: float = 0, iterations: int = 10):
     for i in range(iterations): # Heron's method
         guess = (guess + x / guess) / 2
     return guess
+
+def factorial(x: float):
+    if x == 1 or x == 2:
+        return x
+    return (x * x - x) * factorial(x - 2)
