2a0e8d2f60573ad37e2836794424eb4d8189acce
Dory <pemegomachado@gmail.com>
2025-08-13 15:58:49 +0200

    Added basic selector

--- main.py
+++ main.py
@@ -20,3 +20,29 @@ def pi(iterations: int = 100):
     for i in range(1, iterations):
         sum += 1 / (i * i)
     return square_root(6 * sum, 0, int(iterations / 10))
+
+if __name__ == "__main__":
+    print("Welcome to the Irrational Number Calculator!")
+    print("This calculator will calculate irrational numbers like pi and sqrt(2)!")
+    print("It will do so with iterative algorithms, and will show you how precision changes with each iteration.")
+    print("This is the list of numbers we can calculate (enter q to quit):")
+    print("")
+    print("1) Square Root of 2")
+    print("2) Pi")
+
+    response = input("")
+    match response:
+        case "q":
+            exit()
+        case "1":
+            i = 1
+            print("We're gonna claculate the square root of 2 using Heron's method (initial guess of 0)")
+            while True:
+                print(f"{i} iterations: {square_root(2, 0, i)}")
+                i *= 10
+        case "2":
+            i = 10
+            print("We're gonna claculate pi using the sum of the reciprocals of the squares of the first n natural numbers")
+            while True:
+                print(f"{i} iterations: {pi(i)}")
+                i *= 10
